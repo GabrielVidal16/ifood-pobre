e7c113319ec37a6f25be1cf9ffad5a1df99c7ae7
GabrielVidal16 <gabrielvtxs@gmail.com>
2024-10-10 15:57:35 -0300

    opa

--- import/interface.py
+++
@@ -1,9 +0,0 @@
-def exibir_menu():
-    print("Escolha uma operação:")
-    print("1. adicionar tarefa")
-    print("2. retirar tarefa")
-    print("3. ver tarefas")
-    print("4. sair")
-
-def obter_opcao():
-    return int(input("Digite a opção (1-4): "))
